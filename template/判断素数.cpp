13d0dcc835a74333d96291fadecda8143e5233aa
Zhangzqs <i@zhangzqs.cn>
2022-04-08 21:43:34 +0800

    update(template): 更新判断质数

--- template/判断素数.cpp
+++ template/判断素数.cpp
@@ -9,11 +9,30 @@ bool isPrime1(int n) {
     }
     return true;
 }
+
+bool isPrime2(int n) {
+    if (n < 2) return false;
+    int sn = sqrt(n);
+    for (int i = 2; i <= sn; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+bool isPrime3(int n) {
+    if (n < 2) return false;
+    int sn = sqrt(n);
+    for (int i = 2; i <= sn; i++) {
+        if (n % 2 == 0 || n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     for (int i = 0; i < 100; i++) {
-        if (isPrime1(i)) {
-            cout << i << endl;
-        }
+        cout << i << endl;
+        assert(isPrime1(i) == isPrime2(i));
+        assert(isPrime2(i) == isPrime3(i));
     }
     return 0;
 }
